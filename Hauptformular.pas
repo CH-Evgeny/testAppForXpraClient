cbe27bd43a8d4240e3fb19b7ef6d1873073c854f
Evgeny Chechin <evgeny.chechin@landwehr.it>
2023-06-07 16:22:39 +0200

    test app for bug ticket
    https://github.com/Xpra-org/xpra-html5/issues/251

---
+++ Hauptformular.pas
@@ -0,0 +1,31 @@
+unit Hauptformular;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    btn1: TButton;
+    procedure btn1Click(Sender: TObject);
+  private
+    { Private-Deklarationen }
+  public
+    { Public-Deklarationen }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.btn1Click(Sender: TObject);
+begin
+  MessageDlG('Hallo World!', mtInformation, [mbYes, mbNo], 0);
+end;
+
+end.
